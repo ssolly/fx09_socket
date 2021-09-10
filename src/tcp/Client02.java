a82abd62d330a0bc9c197d88bdeab8ab0053ec3c
Choi.SY <sollly0829@gmail.com>
2021-09-10 12:54:19 +0900

    fx09.tcp. Server02 Client02 내용추가(서버 데이터 입력 후 클라이언트에서 출력

--- src/tcp/Client02.java
+++ src/tcp/Client02.java
@@ -1,6 +1,8 @@
 package tcp;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,12 +12,18 @@ public class Client02 {
 	public static void main(String[] args) throws Exception {
 		
 		Socket sock = new Socket("localhost",12345);
+		
+		InputStream is = sock.getInputStream();
+		DataInputStream di = new DataInputStream(is);
+		
 		OutputStream os = sock.getOutputStream();
 		DataOutputStream dos = new DataOutputStream(os);
 		
+		String readData = di.readUTF();
+		System.out.println("서버로부터 입력 확인 : " + readData);
+		
 		System.out.print("서버로 전송할 데이터 입력 : ");
 		String s = new Scanner(System.in).next();
-		
 		dos.writeUTF(s);
 		System.out.println("전송 완료");
 		
--- src/tcp/Server02.java
+++ src/tcp/Server02.java
@@ -1,9 +1,12 @@
 package tcp;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server02 {
 
@@ -14,6 +17,14 @@ public class Server02 {
 		
 		InputStream is = sock.getInputStream();
 		DataInputStream di = new DataInputStream(is);	//다양한 타입을 받을 수 있음
+		
+		OutputStream os = sock.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
+		
+		System.out.print("서버연결 확인 : ");
+		String s = new Scanner(System.in).next();
+		dos.writeUTF(s);
+
 		
 		String readData = di.readUTF();
 		System.out.println("클라이언트로부터 전송받은 데이터 : " + readData);
